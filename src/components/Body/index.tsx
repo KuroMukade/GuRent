cbc6656c380a7ec130f05dc026efbc532dc093bf
Timur <kuromukade@mail.ru>
2022-09-11 23:14:53 +0300

    created layout

---
+++ src/components/Body/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './body.scss';
+
+const index = () => {
+  return (
+    <div className='main'>
+        <div>
+            <h1>
+                <span>Снять</span> просторную квартиру и <span>чувствовать</span> себя как дома?
+            </h1>
+            <h2>С <span>Gu</span>Rent проще простого!</h2>
+            <Link to={'/'}>
+              <button className='button'>
+                  Выбрать жилье
+              </button>            
+            </Link>
+
+        </div>  
+  </div>
+  );
+}
+
+export default index
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
+    return (
+        <header className="header">
+        <div className='header-logo'>
+          <Link to={"/"}>
+            <img src="logotype.png" alt="Gurent logo" />
+          </Link>
+        </div>
+        <div className='header-navigation'>
+          <Link className="link"  to={"/"}>Арендные дома</Link>
+          <Link className="link" to={'/'}>Карты</Link>
+        </div>
+        <a href='/' className='header-phone'>
+          <img className='header-phone__icon' src="phone-icon.png" alt="phone" />
+          <p className="header-phone__text">+7 (917) 911-33-83</p>
+        </a>
+        <div className='header-account'>
+          <a href="/" className="header-account__link">
+            <img className='header-account__icon' src="user.png" alt="user icon" />
+          </a>
+        </div>
+      </header>
+    );
+}
+
+export default Header;
